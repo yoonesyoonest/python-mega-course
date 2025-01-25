2bbde549202b4fdd816f89f1623885f83d7e783d
Mostafa <farvimail@gmail.com>
2025-01-25 10:46:28 -0800

    Add edit button to the window

--- Day34.py
+++ Day34.py
@@ -7,8 +7,11 @@ input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos("todos.txt"), key="todos",
                       enable_events=True, size=[45,15])
+edit_button = sg.Button("Edit")
 
-window = sg.Window('My To-Do App', layout=[[label, input_box, add_button],[list_box]])
+button_labels = ["Close", "Apply"]
+
+window = sg.Window('My To-Do App', layout=[[label, input_box, add_button],[list_box,edit_button]])
 window.read()
 print("hello")
 window.close()
