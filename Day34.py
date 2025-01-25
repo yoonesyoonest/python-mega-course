0e343b080953a64c0fc783e84eea81ff503aaf3b
Mostafa <farvimail@gmail.com>
2025-01-25 10:36:35 -0800

    Add list box to show todos items in window

--- Day34.py
+++ Day34.py
@@ -1,10 +1,14 @@
 import PySimpleGUI as sg
 
+import functions
+
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos("todos.txt"), key="todos",
+                      enable_events=True, size=[45,15])
 
-window = sg.Window('My To-Do App', layout=[[label, input_box], [add_button]])
+window = sg.Window('My To-Do App', layout=[[label, input_box, add_button],[list_box]])
 window.read()
 print("hello")
 window.close()
