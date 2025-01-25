e5fc9b84e81f4a3157636a5c4144d79a86845f04
Mostafa <farvimail@gmail.com>
2025-01-25 10:35:50 -0800

    Add day34 in python course

---
+++ Day34.py
@@ -0,0 +1,10 @@
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do App', layout=[[label, input_box], [add_button]])
+window.read()
+print("hello")
+window.close()
