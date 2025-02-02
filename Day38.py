1252f45b643ac4c3360b4e2659c7a5fbc9abec7c
Mostafa <farvimail@gmail.com>
2025-02-02 11:45:57 -0800

    Add logic to add_todos from PWA version, and unique key for each checkbox

--- Day38.py
+++ Day38.py
@@ -3,12 +3,25 @@ import functions
 
 todos = functions.get_todos("todos.txt")
 
+def add_todos():
+    todo = st.session_state["new_todo"]
+    todos.append(todo+"\n")
+    functions.write_todos(todos,"todos.txt")
+    st.session_state["new_todo"] = ""
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your prokuctivity")
 
-for todo in todos:
-    st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add new todo...")
+for index,todo in enumerate(todos):
+    st.checkbox(todo, key=f"{index}-checkbox")
+
+# st.session_state
+
+st.text_input(label="",placeholder="Add new todo...",
+              on_change=add_todos, key='new_todo')
+
+
 
