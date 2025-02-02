db412db6ab36ae67d47b67c7529febf5bf37dc05
pc-todo-app <farvimail@gmail.com>
2025-02-02 11:09:08 -0800

    Initial Commit streamlit Module

---
+++ Day38.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos("todos.txt")
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your prokuctivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="",placeholder="Add new todo...")
+
